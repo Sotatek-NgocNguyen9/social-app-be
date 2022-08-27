3c316bb99ceb7aedba34383b3e295b6ac17239dc
Ngoc Nguyen <duyngoc.nguyen2001@gmail.com>
2022-08-28 06:11:35 +0700

    Convert absolute path import to relative path for better compatibility

--- src/modules/friend/friend.module.ts
+++ src/modules/friend/friend.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from 'src/model/entities/user.entity';
+import { UserEntity } from '../../model/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { JwtStrategy } from '../auth/strategy/jwt.strategy';
 import { HttpModule } from '@nestjs/axios';
-import { FriendRequestEntity } from 'src/model/entities/friend-request.entity';
+import { FriendRequestEntity } from '../../model/entities/friend-request.entity';
 import { FriendService } from './friend.service';
 import { FriendController } from './friend.controller';
-import { FriendEntity } from 'src/model/entities/friend.entity';
+import { FriendEntity } from '../../model/entities/friend.entity';
 import { UserService } from '../user/user.service';
 import { UserModule } from '../user/user.module';
 
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from 'src/model/entities/user.entity';
-import { UserRepository } from 'src/model/repositories/user.repository';
-import { UserController } from 'src/modules/user/user.controller';
-import { UserService } from 'src/modules/user/user.service';
+import { UserEntity } from '../../model/entities/user.entity';
+import { UserRepository } from '../../model/repositories/user.repository';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 import { AuthenticationService } from '../auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { JwtStrategy } from '../auth/strategy/jwt.strategy';
